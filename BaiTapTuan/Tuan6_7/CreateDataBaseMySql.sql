1e2e779c42ea7b19781f27d3dd251fd4d15e5936
Nguyễn Xuân Lưu <nguyenxuanluubkhn@gmail.com>
2019-11-01 10:41:31 +0700

    Add files via upload

--- BaiTapTuan/Tuan6_7/CreateDataBaseMySql.sql
+++ BaiTapTuan/Tuan6_7/CreateDataBaseMySql.sql
@@ -5,6 +5,7 @@ CREATE TABLE `MonAn` (
   `Ma` int(11) COLLATE utf8_bin NOT NULL AUTO_INCREMENT,
   `Ten` varchar(255) COLLATE utf8_bin NOT NULL,
   `Gia` int(11) COLLATE utf8_bin NOT NULL,
+  `Anh` blob,
   PRIMARY KEY (`Ma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 INSERT INTO `MonAn`(`Ten`,`Gia`) VALUES('Cá Kho Vũ Đại','120000');
